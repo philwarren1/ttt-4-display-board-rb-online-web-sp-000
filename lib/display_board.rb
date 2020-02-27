c9b3057ed65b98b281822c135392373e1ef3ed87
Philip Warren <philwarren1@gmail.com>
2020-02-27 01:31:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+board = [" "," "," "," "," "," "," "," "," "]
+puts row =  "   |   |   "
+break =  "-----------"
+puts = 
+puts row
+puts break
+puts row
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
